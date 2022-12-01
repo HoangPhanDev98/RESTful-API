9a1ee7aee596135f2b1d46f6a047bcf87eff3ade
HoangPhanDev98 <hoangphandev98@gmail.com>
2022-12-01 10:52:47 +0700

    Add cors

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 require("dotenv/config");
 
 // Middle ware
+app.use(cors());
 app.use(bodyParser.json());
 
 // Import the Routes
